dcdf43fd59c977e8b5670d2a1b4c39d13a8fe986
Muratcan Gözüm <murat.can1250@gmail.com>
2024-02-22 21:19:21 +0300

    dagger unit test

---
+++ app/src/test/java/com/muratcangzm/lunargaze/di/AppModuleTest.kt
@@ -0,0 +1,12 @@
+package com.muratcangzm.lunargaze.di
+
+import android.content.Context
+import org.mockito.Mock
+
+class AppModuleTest {
+
+
+    @Mock
+    private lateinit var mockContext: Context
+
+}
